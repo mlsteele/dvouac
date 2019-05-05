dbdad1a9be0afe80541cde5132bc395bf5c0a51e
Miles Steele <miles@milessteele.com>
2019-05-05 13:13:06 -0400

    events

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,20 +1,60 @@
+use crate::*;
+use std::fs::File;
 
-pub struct Keyboard {}
+pub struct Keyboard {
+}
 
 impl Keyboard {
-    pub fn new() -> Self {
-        // evdev::enumerate()
-        //     device.events
-        //     .next
-        //     .code
-        Self{}
-    }
+    pub fn new() -> Self { Self{} }
 
     pub fn next_key(&mut self) -> Result<char> {
-        unimplemented!();
+        let mut devices = Vec::new();
+        if let Ok(dir) = std::fs::read_dir("/dev/input") {
+            for entry in dir {
+                if let Ok(entry) = entry {
+                    if let Ok(dev) = evdev_rs::Device::new_from_fd(File::open(&entry.path())?) {
+                        devices.push(dev)
+                    }
+                }
+            }
+        }
+        if devices.len() == 0 {
+            bail!("no devices opened (may need sudo)");
+        }
+        for (i, dev) in devices.iter().enumerate() {
+            println!("{}: {:?}", i, dev.name())
+        }
+
+        loop {
+            for dev in &devices {
+                if !dev.has_event_pending() {
+                    continue
+                }
+                let (_, ev) = dev.next_event(evdev_rs::ReadFlag::NORMAL | evdev_rs::ReadFlag::BLOCKING)?;
+                println!("Event: time {:?} {}.{}, ++++++++++++++++++++ {}    {} +++++++++++++++",
+                                dev.name(),
+                                ev.time.tv_sec,
+                                ev.time.tv_usec,
+                                ev.event_type,
+                                ev.event_code);
+                use evdev_rs::enums::*;
+                if let EventCode::EV_KEY(key) = ev.event_code {
+                }
+                return Ok('t');
+            }
+        }
+    }
+
+    fn key_to_char(k: evdev_rs::enums::EV_KEY) -> Option<char> {
+        use evdev_rs::enums::EV_KEY::*;
+        match k {
+            KEY_A => Some('a'),
+            _ => None,
+        }
     }
 
     pub fn switch(&mut self, _layout: Layout) {
         unimplemented!();
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod error;
 use error::*;
-use std::fs::File;
+
+mod keyboard;
+use keyboard::*;
 
 #[macro_use] extern crate failure;
 
@@ -13,55 +15,6 @@ fn main() {
 }
 
 fn main2() -> EResult {
-    // let mut devices = evdev::enumerate();
-    // if devices.len() == 0 {
-    //     bail!("evdev listed no devices (may need sudo)");
-    // }
-    // for (i, device) in devices.iter().enumerate() {
-    //     println!("{} {:?}", i, device.name());
-    // }
-    // let device = &mut devices[5];
-    // println!("{:?}", device.name());
-    // for ev in device.events()? {
-    //     println!("{:?}", ev);
-    // }
-    // return EOK;
-
-    let mut devices = Vec::new();
-    if let Ok(dir) = std::fs::read_dir("/dev/input") {
-        for entry in dir {
-            if let Ok(entry) = entry {
-                if let Ok(dev) = evdev_rs::Device::new_from_fd(File::open(&entry.path())?) {
-                    devices.push(dev)
-                }
-            }
-        }
-    }
-    if devices.len() == 0 {
-        bail!("no devices opened (may need sudo)");
-    }
-    for (i, dev) in devices.iter().enumerate() {
-        println!("{}: {:?}", i, dev.name())
-    }
-
-    loop {
-        for dev in &devices {
-            if !dev.has_event_pending() {
-                continue
-            }
-            let a = dev.next_event(evdev_rs::ReadFlag::NORMAL | evdev_rs::ReadFlag::BLOCKING);
-            match a {
-                Ok(k) => println!("Event: time {:?} {}.{}, ++++++++++++++++++++ {} +++++++++++++++",
-                                  dev.name(),
-                                k.1.time.tv_sec,
-                                k.1.time.tv_usec,
-                                k.1.event_type),
-                Err(e) => (),
-            }
-        }
-    }
-    return EOK;
-
     let mut kb = Keyboard::new();
     let mut recognizer = Recognizer::new();
     loop {
@@ -84,16 +37,27 @@ pub enum Layout {
     Dvorak,
 }
 
-pub struct Recognizer {}
+pub struct Recognizer {
+    buf: Option<char>,
+}
 
 impl Recognizer {
-    pub fn new() -> Self { Self{} }
+    pub fn new() -> Self { Self{
+        buf: Default::default(),
+    } }
 
-    pub fn feed(&mut self, _c: char) {
-        unimplemented!();
+    pub fn feed(&mut self, c: char) {
+        self.buf = Some(c);
     }
 
     pub fn recommend(&self) -> Option<Layout> {
-        unimplemented!();
+        if let Some(c) = self.buf {
+            return match c {
+                'q' => Some(Layout::US),
+                'd' => Some(Layout::Dvorak),
+                _ => None,
+            }
+        }
+        None
     }
 }
