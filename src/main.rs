74f53e31db1750b9f2472d0bf2c66c130542c6cf
Miles Steele <miles@milessteele.com>
2019-05-19 13:06:03 -0400

    compile more

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ fn eprint_error(err: &failure::Error) {
     eprintln!("\n{:?}", err.backtrace());
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Layout {
     US,
     Dvorak,
@@ -73,15 +73,25 @@ impl Recognizer {
 
     pub fn feed(&mut self, c: char) {
         self.buf.push_back(c);
+        if self.buf.len() > self.buf_max {
+            self.buf.pop_front();
+        }
+    }
+
+    pub fn feed_str(&mut self, s: &str) {
+        for c in s.chars() {
+            self.feed(c)
+        }
     }
 
     pub fn recommend(&self) -> Option<Layout> {
         let buf = self.buf.iter().collect::<String>();
         let (head, middle, tail) = Self::split_head_middle_tail(&buf);
+        println!("{:?} {:?} {:?}", head, middle, tail);
         let score: i64 = self.suffix(head)
             + middle.iter().map(|w| self.exact(w)).sum::<i64>()
             + self.prefix(tail);
-        if score > 16 {
+        if score > 9 {
             Some(Layout::US)
         } else {
             None
@@ -106,25 +116,71 @@ impl Recognizer {
         if self.words.contains(s) { s.len() as i64 } else { 0 }
     }
 
-    // is a prefix of s a word
+    // Is s the prefix of a word? Returns length of prefix or 0.
     fn prefix(&self, s: &str) -> i64 {
-        Self::str_compounder(s).find_map(|w|
-            if self.words.contains(w) { Some(w.len() as i64) } else { None }
-        ).unwrap_or(0)
+        self.words.iter()
+            .map(|word| if word.starts_with(s) { s.len() as i64 } else { 0 } )
+            .max().unwrap_or(0)
     }
 
     // is a suffix of s a word
     fn suffix(&self, s: &str) -> i64 {
-        Self::str_compounder(Self::rev(s)).find_map(|w|
-            if self.words.contains(Self::rev(w)) { Some(w.len() as i64) } else { None }
-        ).unwrap_or(0)
+        self.words.iter()
+            .map(|word| if word.ends_with(s) { s.len() as i64 } else { 0 } )
+            .max().unwrap_or(0)
     }
 
-    fn str_compounder(s: &str) -> impl Iterator<Item=&String> {
-        s.chars().scan("", |acc, c| Some(&format!("{}{}", acc, c)))
+    // fn prefixes<'a>(s: &'a str) -> impl Iterator<Item=String> + 'a {
+    //     s.chars().scan("", |acc, c| Some(format!("{}{}", acc, c).to_owned()))
+    // }
+
+    // fn rev(s: &str) -> String {
+    //     s.chars().rev().collect::<String>()
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test] fn test_prefix() {
+        let r = Recognizer::new();
+        assert_eq!(0, r.prefix("qwordq"));
+        assert_eq!(0, r.prefix("wordq"));
+        assert_eq!(0, r.prefix("qword"));
+        assert_eq!(3, r.prefix("wor"));
+        assert_eq!(4, r.prefix("word"));
     }
 
-    fn rev(s: &str) -> &str {
-        &s.chars().rev().collect::<String>()
+    #[test] fn test_suffix() {
+        let r = Recognizer::new();
+        assert_eq!(0, r.suffix("qwordq"));
+        assert_eq!(0, r.suffix("wordq"));
+        assert_eq!(0, r.suffix("qword"));
+        assert_eq!(3, r.suffix("ord"));
+        assert_eq!(4, r.suffix("word"));
+    }
+
+    #[test] fn test_recognizer_us_us() {
+        let mut r = Recognizer::new();
+        assert_eq!(None, r.recommend());
+        r.feed_str("w");
+        assert_eq!(None, r.recommend());
+        r.feed_str("ord word word word");
+        assert_eq!(Some(Layout::US), r.recommend());
+    }
+
+    #[test] fn test_recognizer_us_dv() {
+        let mut r = Recognizer::new();
+        assert_eq!(None, r.recommend());
+        r.feed_str(",rpe");
+        assert_eq!(Some(Layout::US), r.recommend());
+    }
+
+    #[test] fn test_recognizer_dv_us() {
+        let mut r = Recognizer::new();
+        assert_eq!(None, r.recommend());
+        r.feed_str(",soh");
+        assert_eq!(Some(Layout::Dvorak), r.recommend());
     }
 }
